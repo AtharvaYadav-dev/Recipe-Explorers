6a3f2c84c550413c07607b27fda6f3db9345c683
NikitaES94 <nikitajathar07@gmail.com>
2026-01-19 12:46:59 +0530

    Fix NodeJS.Timeout type issue in utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -31,16 +31,16 @@ export function truncateText(text: string, maxLength: number): string {
   return text.slice(0, maxLength) + '...';
 }
 
-export function debounce<T extends (...args: any[]) => any>(
+export const debounce = <T extends (...args: any) => any>(
   func: T,
   wait: number
-): (...args: Parameters<T>) => void {
-  let timeout: NodeJS.Timeout;
+): ((...args: Parameters<T>) => void) => {
+  let timeout: ReturnType<typeof setTimeout>;
   return (...args: Parameters<T>) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), wait);
   };
-}
+};
 
 export function generateId(): string {
   return Math.random().toString(36).substr(2, 9);
